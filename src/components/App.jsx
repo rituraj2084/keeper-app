28e343bc817e26807c134b7d932f54b929af36a2
RItu Raj <rajritu2084@gmail.com>
2023-07-06 21:07:48 +0530

    Initial Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import Header from "./Header";
+import Note from "./Note";
+import notes from '../notes'
+import Footer from "./Footer";
+
+function App(){
+    return <div>
+        <Header />
+        {notes.map((note)=>{
+            return <Note
+                key = {note.key} 
+                title = {note.title}
+                content = {note.content}
+            />
+        })}
+        <Footer />
+    </div>
+}
+
+export default App;
