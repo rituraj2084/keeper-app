a6b3741d0286f8bbc3ef77333608ef98a28bfdc9
RItu Raj <rajritu2084@gmail.com>
2023-07-12 10:56:23 +0530

    Adding dynamic functionalities

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,43 @@
-import React from "react";
-
+import React, { useState } from "react";
 import Header from "./Header";
-import Note from "./Note";
-import notes from '../notes'
 import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
 
-function App(){
-    return <div>
-        <Header />
-        {notes.map((note)=>{
-            return <Note
-                key = {note.key} 
-                title = {note.title}
-                content = {note.content}
-            />
-        })}
-        <Footer />
+function App() {
+
+    const [notes, setNotes] = useState([]);
+
+    function addNote(newNote){
+        setNotes(prevNotes => {
+          return [...prevNotes, newNote]
+        })
+    }
+
+    function deleteNote(id){
+      setNotes((prevNotes, index)=>{
+        return prevNotes.filter((noteItem, index) => {
+          return index !== id;
+        })
+      })
+    }
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index)=>{
+        return <Note 
+          key = {index}
+          id = {index}
+          title = {noteItem.title}
+          content = {noteItem.content}
+          onDelete = {deleteNote}
+        />
+      })}
+      {/* <Note key={1} title="Note title" content="Note content" /> */}
+      <Footer />
     </div>
+  );
 }
 
 export default App;
